408d487c970993424e197d935c8493803bc702e2
Dalila Adami <160270814+ForAdami@users.noreply.github.com>
2025-07-05 16:51:58 -0300

    Update Index.jsx

--- livraria-luz-no-mundo-completo/livraria-luz-no-mundo/client/src/pages/Index.jsx
+++ livraria-luz-no-mundo-completo/livraria-luz-no-mundo/client/src/pages/Index.jsx
@@ -1,1 +1,18 @@
-// Arquivo gerado. O conteúdo real será preenchido depois...
+import { Link } from 'react-router-dom';
+import '../styles/global.css';
+
+function Index() {
+  return (
+    <div className="pagina-index">
+      <header className="logo-container">
+        <img src="/img/logo.png" alt="Logomarca da Livraria" />
+        <h1>Livraria Luz no Mundo</h1>
+      </header>
+      <main className="menu-central">
+        <Link to="/home" className="btn">Biblioteca</Link>
+      </main>
+    </div>
+  );
+}
+
+export default Index;
