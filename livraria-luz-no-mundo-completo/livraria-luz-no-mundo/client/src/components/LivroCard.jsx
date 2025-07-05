b8421ba9313ce906503411ae61747631f9445b31
Dalila Adami <160270814+ForAdami@users.noreply.github.com>
2025-07-05 16:53:04 -0300

    Update LivroCard.jsx

--- livraria-luz-no-mundo-completo/livraria-luz-no-mundo/client/src/components/LivroCard.jsx
+++ livraria-luz-no-mundo-completo/livraria-luz-no-mundo/client/src/components/LivroCard.jsx
@@ -1,1 +1,22 @@
-// Arquivo gerado. O conteúdo real será preenchido depois...
+import { useCarrinho } from '../context/CarrinhoContext';
+import '../styles/livroCard.css';
+
+function LivroCard({ livro }) {
+  const { adicionarAoCarrinho } = useCarrinho();
+
+  return (
+    <div className="livros">
+      <img src={livro.imagem} alt={livro.titulo} />
+      <div className="livro-info">
+        <span className="livro-titulo">{livro.titulo}</span>
+        <p className="livro-descricao">{livro.descricao}</p>
+        <h3 className="livro-preco">R$ {livro.preco.toFixed(2)}</h3>
+        <button className="adicionar-no-carrinho" onClick={() => adicionarAoCarrinho(livro)}>
+          Adicionar ao carrinho
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default LivroCard;
