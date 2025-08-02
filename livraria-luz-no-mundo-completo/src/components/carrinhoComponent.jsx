a00a031a9220caef72176a02c3c89c08cf530ef8
Erechiin <erikrodrigues294@gmail.com>
2025-08-01 21:35:45 -0300

    pequenas mudanças e correção de bug no carrinho

--- livraria-luz-no-mundo-completo/src/components/carrinhoComponent.jsx
+++ livraria-luz-no-mundo-completo/src/components/carrinhoComponent.jsx
@@ -2,97 +2,120 @@ import '../styles/carrinho.css'
 import { Navigate, useNavigate } from 'react-router'
 import { useState } from 'react'
 
-function CarrinhoComponent({Carrinho,setCarrinho,Compra, setCompra, Total, setTotal}){
-    const [escondido,setEscondido] = useState(false)
-    
-    function attCompra(el){
-        setCompra((prevCompra)=>{
-            const jaTem = prevCompra.find(item=>item.id==el.id)
-            if(jaTem){
-                return Carrinho
-            }else{
-                return [...prevCompra]
-            }
+function CarrinhoComponent({ Carrinho, setCarrinho, Compra, setCompra, Total, setTotal }) {
+    const [escondido, setEscondido] = useState(false)
+
+    function increase(el) {
+        setCarrinho((prevCarrinho)=>{
+            return prevCarrinho.map(item=>{
+                return item.id==el.id?{...el,quantidade:item.quantidade+1}:item
+            })
         })
 
-    }
-    function increase(el){
-        const prev= [...Carrinho]
-        prev.find(i=>i==el).quantidade +=1
-        setCarrinho(prev)
-        attCompra(el)
-    }
-    function decrease(el){
-        const prev= [...Carrinho]
-        if(prev.find(e=>e.id==el.id).quantidade==1){
-            alert('vc ira apagar o item...... crtz?')
-            setCarrinho(prev.filter(item=>item.id!==el.id))
-            setCompra(Compra.filter(item=>item.id!==el.id))
-        }else{
-            prev.find(i=>i==el).quantidade -=1
-            setCarrinho(prev)
-            attCompra(el)
+        if(el.marked){
+            setCompra((prevCompra)=>{
+                return prevCompra.map(item=>{
+                    return item.id==el.id?{...item,quantidade:item.quantidade+1}:item
+                })
+            })
         }
+
     }
-    function itemMarked(event, el){
-        if(event.target.checked){
-            setCompra([...Compra,el])
-        }else{
-            setCompra(Compra.filter(item=>item.id!==el.id))
+    function decrease(el) {
+        setCarrinho((prevCarrinho)=>{
+            if(el.quantidade==1){
+                return prevCarrinho.filter(item=>item.id!==el.id)
+            }else{
+                return prevCarrinho.map(item=>item.id==el.id?{...item,quantidade:item.quantidade-1}:item)
+            }
+        })
+        
+        if(el.marked){
+            setCompra((prevCompra)=>{
+                if(el.quantidade==1){
+                    return prevCompra.filter(item=>item.id!==el.id)
+                }else{
+                    return prevCompra.map(item=>item.id==el.id?{...item,quantidade:item.quantidade-1}:item)
+                }
+            })
         }
+
+    }
+    function itemMarked(el) {
+        if (!el.marked) {
+            setCarrinho((prevCarrinho) => {
+                prevCarrinho.find(item => item.id == el.id).marked = true
+                return [...prevCarrinho]
+            })
+            setCompra((prevCompra) => {
+                return [...prevCompra,el]
+                
+            })
+            
+            } else {
+                setCarrinho((prevCarrinho) => {
+                    prevCarrinho.find(item => item.id == el.id).marked = false
+                    return [...prevCarrinho]
+                })
+                setCompra((prevCompra)=>{
+                    const compraFiltered = prevCompra.filter(item => item.id !== el.id)
+                    return [...compraFiltered]
+            })
+        }
+
     }
 
     const navigate = useNavigate()
 
-    const CarrinhoMapped = Carrinho.map(el=>(
+    const CarrinhoMapped = Carrinho.map(el => (
         <div className="carrinhoItem" key={el.id}>
-            <input 
+            <input
                 type="checkbox"
                 className='checkItem'
-                data-id={el.id} 
-                checked={Compra.some(item=>item.id==el.id)} 
-                onChange={(event)=>{itemMarked(event, el)}}
+                data-id={el.id}
+                checked={Compra.some(item => item.id == el.id)}
+                onChange={() => { itemMarked(el) }}
             />
             <span>{el.titulo}</span>
             <span>R$ {el.preco.toFixed(2)}</span>
             <div className="quantidadeWrapper">
 
-                <button onClick={()=>decrease(el)}>-</button>
+                <button onClick={() => decrease(el)}>-</button>
 
                 <span className='quantidade'>{el.quantidade}</span>
-                
-                <button onClick={()=>increase(el)}>+</button>
+
+                <button onClick={() => increase(el)}>+</button>
 
             </div>
         </div>
     ))
-    if(Carrinho.length==0){
+    if (Carrinho.length == 0) {
         return (
             <p className='noItemWarning'>Nenhum item no carrinho!</p>
         )
-    }else{
+    } else {
         return (
             <>
-            <div className="carrinhoItensContainer">
-                {CarrinhoMapped}
-            </div>
+                <div className="carrinhoItensContainer">
+                    {CarrinhoMapped}
+                </div>
 
-            <span
-            className={escondido?'selecione-produto-popUp':'hidden'}
-            >Selecione um produto!</span>
+                <span
+                    className={escondido ? 'selecione-produto-popUp' : 'hidden'}
+                >Selecione um produto!</span>
 
-            <div className="compra-container">
-                <span className='total'>TOTAL: R$ {Total.toFixed(2)}</span>
-                <button onClick={()=>{
-                    if(Compra.length!==0){
-                        setEscondido(false)
-                        navigate('/compra')
-                    }else{
-                        setEscondido(true)
+                <div className="compra-container">
+                    <span className='total'>TOTAL: R$ {Total.toFixed(2)}</span>
+                    <button onClick={() => {
+                        if (Compra.length !== 0) {
+                            setEscondido(false)
+                            navigate('/compra')
+                        } else {
+                            setEscondido(true)
+                        }
                     }
-                }
-                }>Continuar</button>
-            </div>
+                    }>Continuar</button>
+                </div>
             </>
         )
     }
